ff8981e7abf8762b262c6f9e66b78269e51007d4
taslimaakterpapia <taslimaakterpapia495@gmail.comm>
2023-05-23 02:32:13 +0600

    edit comite line

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ console.log(process.env.DB_PASS)
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8nylo01.mongodb.net/?retryWrites=true&w=majority`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
